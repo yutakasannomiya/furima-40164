2de5139dbc46fb22d9b67e4350400c2c889f2752
yutakasannomiya <151412157+yutakasannomiya@users.noreply.github.com>
2023-12-07 17:27:32 +0900

    Revert "コミット"
    This reverts commit 96d7551e32a7b99916d950582835edf886cdf66b.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,5 @@
 class ItemsController < ApplicationController
-
-
   def index
+   # @items = Ietem.all
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-
   root to: 'items#index'
 
   # resources :items, only: :index
