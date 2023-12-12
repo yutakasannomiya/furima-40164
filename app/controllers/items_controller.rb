84bb7435c184a9bae08aa764b81a9faa96effbce
yutakasannomiya <151412157+yutakasannomiya@users.noreply.github.com>
2023-12-12 17:20:59 +0900

    商品編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show, :update]
+  before_action :contributor_confirmation, only: [:edit, :update]
 
 
   def index
@@ -20,12 +22,30 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+    redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
   def item_params
     params.require(:item).permit(:image, :item_name, :item_text, :category_id, :condition_id, :shipping_id, :pref_id, :scheduled_day_id, :item_price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path if current_user.id != @item.user_id 
   end
 
 end
